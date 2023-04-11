084e0b85665ab58d347337351640b1b1b46fd3e5
seanmisra <seanmisra30@gmail.com>
2023-04-11 17:30:28 -0400

    Exercise 2: demonstrate difference between var and let

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("testing");
+const flag = true;
+let firstVariable;
+var secondVariable;
+
+if (flag) {
+    firstVariable = "a";
+}
+
+if (flag) {
+    secondVariable = "b";
+}
+
+console.log(firstVariable);
+console.log(secondVariable);
