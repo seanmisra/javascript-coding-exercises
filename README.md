aeeb731c9dc173596c39ca735b223662e98e72aa
seanmisra <seanmisra30@gmail.com>
2023-04-11 17:47:50 -0400

    Exercise 4: demonstrate diff btw == and ===

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Commits correspond to each exercise
 1. HTML/JS set up
 2. Demonstrate difference between var/let (function vs block scope)
 3. Example of using a rest parameter (pass values individually and using spread syntax)
+4. Diff btw == and ===
--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// pass via individual values
-restTest("a", "b", "c", "d", "e");
+// CONDITION 1 and 3 will pass, 2 will fail
 
-// pass via array/spread syntax
-let letters = ["a", "b", "c", "d", "e"];
-restTest(...letters);
+if ("3" == 3) {
+    console.log("CONDITION 1 PASSED");
+} else {
+    console.log("CONDITION 1 FAILED");
+}
 
-function restTest(...letters) {
-    letters.forEach(char => console.log(char));
+if ("3" === 3) {
+    console.log("CONDITION 2 PASSED");
+} else {
+    console.log("CONDITION 2 FAILED");
+}
+
+if (3 === 3) {
+    console.log("CONDITION 3 PASSED");
+} else {
+    console.log("CONDITION 3 FAILED");
 }
